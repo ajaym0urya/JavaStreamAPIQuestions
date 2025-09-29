4436a0919f23d45ccbc3f34aa9459721a1cbb504
Ajay Mourya <ajaxmourya@gmail.com>
2025-09-29 18:41:28 +0530

    Basic Java Stream API Operations Questions

---
+++ src/Easy/CountNumberOfUppercaseLetters.java
@@ -0,0 +1,18 @@
+package Easy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CountNumberOfUppercaseLetters {
+    public static void main(String[] args) {
+        // Input: "JavaStreamAPI"
+        // Output: 5
+
+        String str="JavaStreamAPI" ;
+        long count=  str.chars()           // Convert int to Character
+                .filter(Character::isUpperCase)
+                .count();
+
+        System.out.println(count);
+    }
+}
---
+++ src/Easy/FilterVowelsFromAString.java
@@ -0,0 +1,19 @@
+package Easy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterVowelsFromAString {
+    public static void main(String[] args) {
+        // Input:  "streamapi"
+        // Output: [e, a, a, i]
+
+        String str="streamapi" ;
+        List<Character> list=  str.chars()
+                .mapToObj(c -> (char) c)            // Convert int to Character
+                .filter(s -> s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u')
+                .toList();
+
+        System.out.println(list);
+    }
+}
